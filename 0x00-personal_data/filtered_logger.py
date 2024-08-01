ae050097f82955b2937ea1782b21b6dd0396ba4c
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-01 15:25:38 +0200

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
-"""
-A module for filtering logs.
-
-"""
+""" A module for filtering logs. """
 
 import re
+import os
 from typing import List
 import logging
-import os
 import mysql.connector
 from logging import StreamHandler
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """
-    Filters a log line.
-    """
-    for field in fields:
-        message = re.sub(rf"{re.escape(field)}=(.*?){re.escape(separator)}",
-                         f"{field}={redaction}{separator}", message)
-    return message
-
-
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -38,40 +24,46 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """formats a LogRecord.
-        """
-        msg = super(RedactingFormatter, self).format(record)
-        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
-        return txt
+        """ Formats the log record """
+        mess = super(RedactingFormatter, self).format(record)
+        form = filter_datum(self.fields, self.REDACTION, mess, self.SEPARATOR)
+        return form
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Filters a log line. """
+    for field in fields:
+        message = re.sub(rf"{re.escape(field)}=(.*?){re.escape(separator)}",
+                         f"{field}={redaction}{separator}", message)
+    return message
 
 
 def get_logger() -> logging.Logger:
-    """Creates a new logger for user data.
-    """
+    """ Creates a new logger. """
     logger = logging.getLogger("user_data")
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    logger.addHandler(stream_handler)
+    logger.addHandler(handler)
     return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Creates a connector to a database.
-    """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
-        host=db_host,
+    """ Returns a connector to the database """
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    conn = mysql.connector.connect(
+        host=host,
         port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
+        user=user,
+        password=password,
+        database=name,
     )
-    return connection
+    return conn
 
 
 def main() -> None:
@@ -79,16 +71,12 @@ def main() -> None:
     Main function to retrieve data from database
     """
     db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    for row in cursor:
+    mess = db.cursor()
+    mess.execute("SELECT * FROM users;")
+    for row in mess:
         message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
             f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
             f"last_login={row[6]}; user_agent={row[7]};"
         print(message)
-    cursor.close()
+    mess.close()
     db.close()
-
-
-if __name__ == '__main__':
-    main()
