2d112dbc80e564e243e10b8844f6b82e5470e70c
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-07 20:22:11 +0200

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,13 +34,11 @@ else:
 
 @app.before_request
 def handle_before_request() -> None:
-    """runs before each request"""
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/auth_session/login/',
-        '/api/v1/forbidden/'
-    ]
+    """Before request"""
+    auth_req = ['/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/auth_session/login/',
+                '/api/v1/forbidden/']
     if auth:
         if auth.require_auth(request.path, excluded_paths):
             if (not auth.authorization_header(request) and
@@ -55,26 +53,23 @@ def handle_before_request() -> None:
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
