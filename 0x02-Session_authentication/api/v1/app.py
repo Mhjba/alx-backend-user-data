1f2b8bf615dbc90399389bcc74746520bacf377d
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-07 20:18:28 +0200

    session authication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,15 +3,10 @@
 Route module for the API
 """
 from os import getenv
-import os
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
-
+import os
 
 
 app = Flask(__name__)
@@ -21,12 +16,16 @@ auth = None
 auth = getenv("AUTH_TYPE")
 
 if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif auth == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 elif auth == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 elif auth == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 else:
     from api.v1.auth.auth import Auth
@@ -35,11 +34,13 @@ else:
 
 @app.before_request
 def handle_before_request() -> None:
-    """Before request"""
-    auth_req = ['/api/v1/status/',
-                '/api/v1/unauthorized/',
-                '/api/v1/auth_session/login/',
-                '/api/v1/forbidden/']
+    """runs before each request"""
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/auth_session/login/',
+        '/api/v1/forbidden/'
+    ]
     if auth:
         if auth.require_auth(request.path, excluded_paths):
             if (not auth.authorization_header(request) and
@@ -54,23 +55,26 @@ def handle_before_request() -> None:
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
