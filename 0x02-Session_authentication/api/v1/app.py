3e023a6f328dff529f29813fb03371bc92662ff7
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-07 20:57:37 +0200

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,15 +18,6 @@ auth = getenv("AUTH_TYPE")
 if auth == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif auth == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
-elif auth == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -36,36 +27,30 @@ else:
 def handle_before_request() -> None:
     """Before request"""
     auth_req = ['/api/v1/status/',
-                '/api/v1/unauthorized/',
-                '/api/v1/auth_session/login/',
-                '/api/v1/forbidden/']
+                 '/api/v1/unauthorized/',
+                 '/api/v1/forbidden/']
     if auth:
-        if auth.require_auth(request.path, excluded_paths):
-            if (not auth.authorization_header(request) and
-                    not auth.session_cookie(request)):
+        if auth.require_auth(request.path, auth_req):
+            if not auth.authorization_header(request):
                 abort(401)
-            # if not auth.authorization_header(request):
-            #     abort(401)
             if not auth.current_user(request):
                 abort(403)
-            request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
-    return jsonify({"error": "Not found"}), 404
-
+    """ Not found handler"""
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Not found handler """
+    """ Not found handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
